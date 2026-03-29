b04e2497c31c4280413f590bedea08756dcfb203
furqanrupom <furqanrupom978@gmail.com>
2026-03-29 23:42:08 +0600

    fix: port issue

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v1')
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 
 bootstrap();
